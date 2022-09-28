a45b1da27697aab8b909cd7762db1556aa65cae2
Julian Ewers <ewerspej@tcd.ie>
2022-09-28 09:23:03 +0200

    added screenshot; formatted code

--- dotnet-maui-bug-repro-ios-tabs-shell/dotnet-maui-bug-repro-ios-tabs-shell/MauiProgram.cs
+++ dotnet-maui-bug-repro-ios-tabs-shell/dotnet-maui-bug-repro-ios-tabs-shell/MauiProgram.cs
@@ -4,22 +4,22 @@ namespace dotnet_maui_bug_repro_ios_tabs_shell;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 fonts.AddFont("materialdesignicons-webfont.ttf", "materialdesignicons");
             });
 
 #if DEBUG
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
